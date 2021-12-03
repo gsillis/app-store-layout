0c31d03bd782799fcbae0f57b11a2f9d1c91a3e7
Gabriela Sillis <gabydias47@gmail.com>
2021-12-03 15:52:24 -0300

    initial commit

---
+++ AppStoreLayout/AppStoreLayout/View/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AppStoreLayout
+//
+//  Created by Gabriela Sillis on 03/12/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ AppStoreLayout/UIPreview/UIPreview.h
@@ -0,0 +1,18 @@
+//
+//  UIPreview.h
+//  UIPreview
+//
+//  Created by Gabriela Sillis on 03/12/21.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for UIPreview.
+FOUNDATION_EXPORT double UIPreviewVersionNumber;
+
+//! Project version string for UIPreview.
+FOUNDATION_EXPORT const unsigned char UIPreviewVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <UIPreview/PublicHeader.h>
+
+
