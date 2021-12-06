e0e1f9c3b4c0066012a9b8a4a1ee08717fa8dcd4
Gabriela Sillis <gabydias47@gmail.com>
2021-12-06 17:36:57 -0300

    custom cell created

---
+++ appStoreLayout/appStoreLayout/Utils/CollectionViewCell+Extensions.swift
@@ -0,0 +1,16 @@
+//
+//  CollectionView+Extensions.swift
+//  appStoreLayout
+//
+//  Created by Gabriela Sillis on 06/12/21.
+//
+
+import UIKit
+
+extension UICollectionViewCell {
+    static var identifier: String {
+        return String(describing: self)
+    }
+}
+
+
